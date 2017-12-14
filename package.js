4f42c820691df00cf739602860692aeafb9656d0
Kelly Copley <copleykj@gmail.com>
2017-12-14 16:08:34 -0500

    v1.0.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:server-presence',
     summary: 'Scalable server presence',
-    version: '1.0.0',
+    version: '1.0.1',
     git: 'https://github.com/copleykj/socialize-server-presence.git',
 });
 
