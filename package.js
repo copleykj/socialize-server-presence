89ffd6c9073386737ef8d769ddb92d6923d6d227
Kelly Copley <copleykj@gmail.com>
2023-12-06 10:22:55 -0500

    v1.0.5

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 /* global Package */
 Package.describe({
-    name: 'socialize:server-presence',
-    summary: 'Scalable server presence',
-    version: '1.0.4',
-    git: 'https://github.com/copleykj/socialize-server-presence.git',
+  name: 'socialize:server-presence',
+  summary: 'Scalable server presence',
+  version: '1.0.5',
+  git: 'https://github.com/copleykj/socialize-server-presence.git',
 });
 
-Package.onUse(function _(api) {
-    api.versionsFrom(['1.10.2', '2.3']);
+Package.onUse(function _ (api) {
+  api.versionsFrom(['1.10.2', '2.3']);
 
-    api.use(['ecmascript', 'mongo'], 'server');
+  api.use(['ecmascript', 'mongo'], 'server');
 
-    api.mainModule('server-presence.js', 'server');
+  api.mainModule('server-presence.js', 'server');
 });
